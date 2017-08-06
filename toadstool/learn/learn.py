3d2a1429dc6a22a3f8db8aa4ff6ff8e3d99b1f16
Sean Helm <shelm@gmu.edu>
2017-08-05 22:59:31 -0400

    Added documentation to MultiClassifier

--- toadstool/learn/learn.py
+++ toadstool/learn/learn.py
@@ -1,3 +1,6 @@
+""""
+Supervised learning classification
+""""
 import pandas as pd
 
 from sklearn import preprocessing
@@ -8,7 +11,12 @@ __all__ = 'MultiClassifier'
 
 
 class MultiClassifier:
-    '''Uses multiple statistical classification models to classify data'''
+    """
+    Uses multiple statistical classification models to classify data
+
+    :param data: Pandas DataFrame dataset
+    :param models: Dictionary of classificiation models
+    """
     def __init__(self, data, models):
         self.data = data
         self.models = models
@@ -16,7 +24,9 @@ class MultiClassifier:
         self.processed = {}
 
     def preprocess(self):
-        '''Preprocesses the dataset for training'''
+        """
+        Preprocesses the dataset for training
+        """
         for column in self.data.columns:
             # Encode all labels to value between 0..n-1
             self.encoders[column] = preprocessing.LabelEncoder()
@@ -25,18 +35,25 @@ class MultiClassifier:
         # Split data into train and test
         data_X, data_y = self.data.iloc[:, 1:], self.data.iloc[:, 0]
         split_data = train_test_split(data_X, data_y)
+        
         self.processed['train_X'] = split_data[0]
         self.processed['test_X'] = split_data[1]
         self.processed['train_y'] = split_data[2]
         self.processed['test_y'] = split_data[3]
 
     def train_all(self):
-        '''Train the dataset on each statistical model'''
+        """
+        Train the dataset on each statistical model
+        """
         for model in self.models.values():
             model.fit(self.processed['train_X'], self.processed['train_y'])
 
     def test_all(self):
-        '''Test the statistical models on the test data and output its accuracy'''
+        """
+        Test the statistical models on the test data and output its accuracy
+        
+        :returns: Accuracy score for each model (0.0-1.0)
+        """
         accuracy = {}
 
         for key, model in self.models.items():
@@ -46,7 +63,12 @@ class MultiClassifier:
         return accuracy
 
     def predict_new(self, item):
-        '''Classify new item based on trained data'''
+        """
+        Classify new item based on trained data
+
+        :param item: Pandas DataFrame for prediction
+        :returns: Dictionary of predictions from each model
+        """
         for column in item.columns:
             item[column] = self.encoders[column].transform(item[column])
     
--- toadstool/learn/run.py
+++ toadstool/learn/run.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
