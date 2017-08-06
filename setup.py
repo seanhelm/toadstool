b52c2d71d376f2b2f7776c5175fc6e311310c376
Sean Helm <shelm@gmu.edu>
2017-08-06 17:10:33 -0400

    Added setup.py to install module

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='toadstool',
+    version='1.0',
+    author='Sean Helm',
+    author_email='seanmdhelm@gmail.com'
+    packages=['toadstool']
+)
